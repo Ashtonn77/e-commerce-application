b76a1388e0e5fe8a212ec601bea44865eaa158b1
Ashton Naidoo <ashtonrbay@gmail.com>
2021-02-26 13:30:16 +0200

    added angular routes to each page

--- ecommerce_project/03-front-end/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce_project/03-front-end/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list-table.component.html',
-  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products : Product[];
 
-  constructor(private productService: ProductService) { }
+  currentCategoryId : number;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit()
    {
-
-    this.listProducts();
+    this.route.params.subscribe(() => {
+      this.listProducts();
+    });   
 
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    //check if id is available
+    const hasCategory: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategory)
+    {
+      //get the id which is a string - convert to number
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else
+    {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
