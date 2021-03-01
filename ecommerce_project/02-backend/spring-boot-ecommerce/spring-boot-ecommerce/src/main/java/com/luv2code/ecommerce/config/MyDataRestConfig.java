dadf2d9258fbadc583694c34be88d84cfe684e63
Ashton Naidoo <ashtonrbay@gmail.com>
2021-03-01 13:19:15 +0200

    dynamically added product categories

--- ecommerce_project/02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ ecommerce_project/02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -1,15 +1,34 @@
 package com.luv2code.ecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
 
 @Configuration 
 public class MyDataRestConfig implements RepositoryRestConfigurer{
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager)
+	{
+		entityManager = theEntityManager;
+	};
+
+	@Autowired
+
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -26,8 +45,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
 				config.getExposureConfiguration()
 				.forDomainType(ProductCategory.class)
 				.withItemExposure((metData, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metData, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-		
+				.withCollectionExposure((metData, httpMethods) -> httpMethods.disable(theUnsupportedActions));		
+
+
+		//  call internal helper to help expose ids
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+
+		// get list of all entity classes from entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+		// create array of entity types
+		List<Class> entityClasses = new ArrayList<>();
+
+		for(EntityType tmpEntityType : entities)
+		{
+			entityClasses.add(tmpEntityType.getJavaType());
+		}
+
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
+
 	}
 
 	
