6eac77b397ece796626abb827dc48d2f08282b75
Ashton Naidoo <ashtonrbay@gmail.com>
2021-03-09 13:15:49 +0200

    added cart component and functionality

---
+++ ecommerce_project/03-front-end/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if(this.cartItems.length > 0){
+      
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id == theCartItem.id);
+      alreadyExistsInCart = (existingCartItem != undefined);
+
+    }
+
+    if(alreadyExistsInCart){
+
+      existingCartItem.quantity++;
+
+    } else {
+
+      this.cartItems.push(theCartItem);
+
+    }
+
+    this.computeCartTotals();
+
+  }
+
+  computeCartTotals() {
+
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let currentCartItem of this.cartItems){
+
+        totalPriceValue += (currentCartItem.quantity * currentCartItem.unitPrice);
+        totalQuantityValue += currentCartItem.quantity;
+
+    }
+
+    //publish new values - all subscribers will recieve the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartDetails(totalPriceValue, totalQuantityValue);
+
+  }
+  logCartDetails(totalPriceValue: number, totalQuantityValue: number) {
+
+    console.log("Contents of cart: ");
+
+    for(let cartItem of this.cartItems){
+
+        const subTotal = cartItem.quantity * cartItem.unitPrice;
+        console.log(`Name: ${cartItem.name}\nPrice: ${cartItem.unitPrice}\nQuantity: ${cartItem.quantity}\nSubtotal: ${subTotal}`)
+
+    }
+
+    console.log(`Total quantity: ${totalQuantityValue}\nTotal price: R${totalPriceValue}`)
+    console.log("----------------------------------------")
+  }
+
+}
