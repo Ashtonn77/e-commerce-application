e30bd2b42b8858bfef5983f86957a801b21acfd5
Ashton Naidoo <ashtonrbay@gmail.com>
2021-03-19 13:04:17 +0200

    added checkout functionality - db

---
+++ ecommerce_project/02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,23 @@
+package com.luv2code.ecommerce.dto;
+
+public class PurchaseResponse {
+
+	private String orderTrackingNumber;
+
+		
+	public PurchaseResponse(String orderTrackingNumber) {
+		super();
+		this.orderTrackingNumber = orderTrackingNumber;
+	}
+
+	public String getOrderTrackingNumber() {
+		return orderTrackingNumber;
+	}
+
+	public void setOrderTrackingNumber(String orderTrackingNumber) {
+		this.orderTrackingNumber = orderTrackingNumber;
+	}
+	
+	
+		
+}
